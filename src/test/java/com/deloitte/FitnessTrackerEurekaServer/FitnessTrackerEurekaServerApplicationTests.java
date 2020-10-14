f07168cc4c867ad9cb480ddf89191918fa66e112
amitkugupta08 <amitkugupta@deloitte.com>
2020-10-14 13:17:43 +0530

    development/ft_eurekaServer : initial commit for the eureka sercer service.

---
+++ src/test/java/com/deloitte/FitnessTrackerEurekaServer/FitnessTrackerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.FitnessTrackerEurekaServer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FitnessTrackerEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
