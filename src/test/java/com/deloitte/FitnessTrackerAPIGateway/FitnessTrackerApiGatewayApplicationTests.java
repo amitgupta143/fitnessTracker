bda980f480cd7695a6981f771378985a4fbf6086
amitkugupta08 <amitkugupta@deloitte.com>
2020-10-14 13:16:15 +0530

    development/ft_apiGateway : initial commit for the API Gateway service.

---
+++ src/test/java/com/deloitte/FitnessTrackerAPIGateway/FitnessTrackerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.FitnessTrackerAPIGateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FitnessTrackerApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
